69ce4cf264e57d06cd3c2bc5a4e898dae7def7fd
NeoRazorX <neorazorx@gmail.com>
2018-08-29 19:42:47 +0200

    Actualizado el plugin para hacer uso de las últimas mejoras en facturaScripts 2017.904.

--- controller/admin_kdb.php
+++ controller/admin_kdb.php
@@ -1,87 +1,1 @@
-<?php
-require_once 'plugins/facturacion_base/extras/fbase_controller.php';
-/// la clase se debe llamar igual que el archivo
-class admin_kdb extends fbase_controller {
-
-  public $kdb;
-  public $resultados;
-  public $offset;
-  public $total_resultados;
-
-  public function __construct() {
-    /// se crea una entrada 'Mi controlador' dentro del menú 'Mio'
-    parent::__construct(__CLASS__, 'Entrada', 'KDB');
-  }
-
-  protected function private_core() {
-    parent::private_core();
-    $this->kdb = new kdb();
-
-    if (isset($_POST['ssintoma'])){
-      $this->nuevo_kdb();
-    }else if (isset($_GET['delete'])){
-      $this->eliminar_kdb();
-    }
-
-    $this->ini_filters();
-    $this->buscar();
-  }
-
-  private function ini_filters() {
-    $this->offset = 0;
-    if (isset($_GET['offset'])) {
-      $this->offset = intval($_GET['offset']);
-    }
-  }
-
-  private function nuevo_kdb(){
-    $kdb0= new kdb();
-    $kdb0->idkdb = $kdb0->get_new_codigo();
-    $kdb0->sintoma = $_POST['ssintoma'];
-    $kdb0->causa = $_POST['scausa'];
-    $kdb0->solucion = $_POST['ssolucion'];
-    $kdb0->observaciones = $_POST['sobservaciones'];
-    if ($kdb0->save()) {
-      $this->new_message("Entrada " . $age0->codagente . " guardada correctamente.");
-      header('location: ' . $kdb0->url());
-    } else {
-      $this->new_error_msg("¡Imposible guardar la entrada!");
-    }
-  }
-
-  private function eliminar_kdb(){
-    $kdb0 = $this->kdb->get($_GET['delete']);
-    if ($kdb0) {
-      if ($kdb0->delete()) {
-        $this->new_message("Entrada " . $kdb0->idkdb . " eliminada correctamente.");
-      } else {
-        $this->new_error_msg("¡Imposible eliminar la entrada!");
-      }
-    } else {
-      $this->new_error_msg("¡Entrada no encontrado!");
-    }
-  }
-
-  public function paginas() {
-      $url = $this->url() . "&query=" . $this->query;
-      return $this->fbase_paginas($url, $this->total_resultados, $this->offset);
-  }
-
-  private function buscar() {
-    $this->total_resultados = 0;
-    $query = $this->query;
-
-    $data = $this->db->select("SELECT COUNT(idkdb) as total FROM kdb WHERE SINTOMA LIKE '%" . $query . "%'");
-    if ($data) {
-      $this->total_resultados = intval($data[0]['total']);
-    }
-
-    $data2 = $this->db->select("SELECT * FROM kdb WHERE SINTOMA LIKE '%" . $query . "%'");
-    if ($data2) {
-      foreach ($data2 as $d) {
-        $this->resultados[] = new kdb($d);
-      }
-    }
-  }
-
- }
+<?php/// la clase se debe llamar igual que el archivoclass admin_kdb extends fs_list_controller{    public function __construct()    {        /// se crea una entrada 'Mi controlador' dentro del menú 'Mio'        parent::__construct(__CLASS__, 'Entrada', 'KDB');    }    protected function create_tabs()    {        $this->add_tab('kdb', 'entradas', 'kdb');        $this->add_search_columns('kdb', ['causa','sintoma','solucion','observaciones']);        $this->add_sort_option('kdb', ['idkdb']);        $this->add_sort_option('kdb', ['fecha']);        $this->add_sort_option('kdb', ['sintoma'], 1);                /// filtros        $this->add_filter_date('kdb', 'fecha', 'desde', '>=');        $this->add_filter_date('kdb', 'fecha', 'hasta', '<=');                /// columnas a mostrar        $this->decoration->add_column('kdb', 'sintoma');        $this->decoration->add_column('kdb', 'causa');        $this->decoration->add_column('kdb', 'solucion');        $this->decoration->add_column('kdb', 'fecha', 'date', 'fecha', 'text-right');        $this->decoration->add_row_url('kdb', 'index.php?page=edit_kdb&code=', 'idkdb');                /// colores        $this->decoration->add_row_option('kdb', 'fecha', null, 'warning');                /// botones        $this->add_button('kdb', 'nuevo', 'index.php?page=edit_kdb', 'fa-plus', 'btn-success');    }}
--- controller/edit_kdb.php
+++ controller/edit_kdb.php
@@ -1,51 +1,1 @@
-<?php
-/// la clase se debe llamar igual que el archivo
-class edit_kdb extends fs_controller {
-
-  public $kdb;
-
-  public function __construct() {
-    /// se crea una entrada 'Mi controlador' dentro del menú 'Mio'
-    parent::__construct(__CLASS__, 'Entrada', 'KDB', FALSE, FALSE);
-  }
-
-  protected function private_core() {
-    parent::private_core();
-    $this->ppage = $this->page->get('admin_kdb');
-    $this->kdb = FALSE;
-    if (isset($_GET['cod'])) {
-      $kdb = new kdb();
-      $this->kdb = $kdb->get($_GET['cod']);
-    }
-
-    if(isset($_POST['sintoma'])){
-      $this->modificar();
-    }
-
-  }
-
-  private function modificar() {
-    if ($this->kdb) {
-      $this->kdb->sintoma = $_POST['sintoma'];
-      $this->kdb->causa = $_POST['causa'];
-      $this->kdb->solucion = $_POST['solucion'];
-      $this->kdb->observaciones = $_POST['observaciones'];
-
-      if ($this->kdb->save()) {
-        $this->new_message("Datos guardados correctamente.");
-      } else {
-        $this->new_error_msg("¡Imposible guardar!");
-      }
-    }
-  }
-
-  public function url() {
-    if (!isset($this->kdb)) {
-      return parent::url();
-    } else if ($this->kdb) {
-      return $this->kdb->url();
-    }
-    return $this->page->url();
-  }
-
-}
+<?php/// la clase se debe llamar igual que el archivoclass edit_kdb extends fs_edit_controller{    public function __construct()    {        /// se crea una entrada 'Mi controlador' dentro del menú 'Mio'        parent::__construct(__CLASS__, 'Entrada', 'KDB', FALSE, FALSE);    }    public function get_model_class_name()    {        return 'kdb';    }    protected function set_edit_columns()    {        $this->form->add_column('sintoma', 'textarea', 'sintoma', 6, true);        $this->form->add_column('causa', 'textarea', 'causa', 6);        $this->form->add_column('solucion', 'textarea', 'solución', 12, true);        $this->form->add_column('observaciones', 'textarea', 'obsevaciones', 6);        $this->form->add_column('fecha', 'date', 'fecha');    }}
